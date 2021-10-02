d043d8ef0a6fe160256faf237385e8af514f16a7
Skymaster640 <skylar.TheHouseRobinson@gmail.com>
2021-10-02 17:09:25 -0500

    Add files via upload

---
+++ datatransfer.py
@@ -0,0 +1,20 @@
+
+def SwapFile():
+
+    file1 = "C:\Users\skyla\Downloads\Project98\sample1.txt"
+    file2 = "C:\Users\skyla\Downloads\Project98\sample2.txt"
+
+
+
+    with open(file1,'r') as a:
+        data_a = a.read()
+    with open(file2,'r') as b:
+        data_b = b.read()
+    with open(file1,'w') as a:
+        a.write(data_b)
+    with open(file2,'w') as b:
+        b.write(data_a)
+
+
+
+SwapFile()
